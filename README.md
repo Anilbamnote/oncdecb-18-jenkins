cc4ba0bc1b51bee99363d47e6daf57d3c5a78ab8
Anil bhau <anil@gmail.com>
2025-02-20 12:45:12 +0530

    jenkins-code-added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # oncdecb-18-jenkins
 jenkis-projects
+Pipeline
+Automation of the JOBs
+
+4 Jobs - Pipeline PULL -> BUILD -> TEST -> DEPLOYMENT (CICD) Plugins (Pipeline)
+
+Pipeline -> Groovy Language (DSL)Domain-Specific Language, which is a scripting language used to define and manage Jenkins jobs.
+
+ Scripted Pipeline .groovy | node Declarative Pipeline .groovy/.jdp
+  | More easy to Use | pipeline
+
---
+++ declarative-pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage('git_checkout') {
+            steps {
+                git branch: 'develop', url: 'https://github.com/cloud-blitz/angular-java.git'
+            }
+        }
+        stage('build') {
+            steps {
+                echo "buils success"
+            }
+        }
+        stage('test') {
+            steps {
+                echo "test success"
+            }
+        }
+        stage('deploy') {
+            steps {
+                echo "depoly success"
+            }
+        }
+    }
+}
