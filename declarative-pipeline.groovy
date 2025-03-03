90065d805a82d4f9620ef189284a4e7f27c85cad
Anil bhau <anil@gmail.com>
2025-03-03 20:37:25 +0530

    jenkins-code-added

--- declarative-pipeline.groovy
+++ declarative-pipeline.groovy
@@ -28,11 +28,17 @@ pipeline {
                  waitForQualityGate true
             }
         }
-        stage('deploy') {
+         stage('artifact-upload') {
             steps {
-                deploy adapters: [tomcat9(credentialsId: 'tomacat-cred', path: '', url: 'http://47.128.241.201:8080')], contextPath: '/', war: '**/*.war'
-            }
+               sh 'aws s3 cp target/studentapp-2.2-SNAPSHOT.war  s3://my-terra-bucket00999'
+           }
         }
+    }
+        // stage('deploy') {
+        //     steps {
+        //         deploy adapters: [tomcat9(credentialsId: 'tomacat-cred', path: '', url: 'http://47.128.241.201:8080')], contextPath: '/', war: '**/*.war'
+        //     }
+        // }
     }
 }
 
